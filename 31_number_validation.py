eff2244efa83a81155088c31e4cc13396130bc0b
Matias <matiasborba89@gmail.com>
2023-06-05 18:40:22 -0300

    number validations

--- 31_number_validation.py
+++ 31_number_validation.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+# el ge = 1 es: es una abreviatura de "greater than or equal" 
+
+# lt (less than): verifica que el valor sea menor que el valor especificado.
+# le (less than or equal): verifica que el valor sea menor o igual que el valor especificado.
+# gt (greater than): verifica que el valor sea mayor que el valor especificado.
+# ne (not equal): verifica que el valor no sea igual al valor especificado.
+# ge (greater than or equal): verifica que el valor sea mayor o igual que el valor especificado.
 
 @app.get('/items/{item_id}')
-async def read_items(*, item_id: int = Path(title='The ID of the item to get', ge=1), q: str):
+async def read_items(*, item_id: int = Path(title='The ID of the item to get', lt=1), q: str):
     results = {'item_id': item_id}
 
     if q:
