a1b35afca8785a32ce4ec3c00ae85a8b62176f0d
Matias <matiasborba89@gmail.com>
2023-06-09 18:51:35 -0300

    learning +

--- 70_intro_form.py
+++ 70_intro_form.py
@@ -4,6 +4,7 @@ app = FastAPI()
 
 # aca no podemos enviar parametros en json, tienen que ser en formato Form
 # NO SE PUEDE USAR JSON PARA ENVIAR USERNAME/PASSWORDDDD
+# EL FORM SE USA CON EL PROTOCOLO HTTP, o sea para los datos que vienen desde un formulario, ej: login, crear usuario, etc
 
 @app.post("/login/")
 async def login(username: str = Form(), password: str = Form()):
