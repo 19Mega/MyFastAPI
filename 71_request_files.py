68967e73a3393cd794790c8e4e2e57bb4e70ebc8
Matias <matiasborba89@gmail.com>
2023-06-10 17:16:00 -0300

    learning +

--- 71_request_files.py
+++ 71_request_files.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
+# el objeto File() se guarda en memoria como un array de bytes, tambien podemos
+# acceder a los metadatos de este archivo mediante File(), por ejemplo len(file)
+# return {"filename": file.filename, "filesize": file.size, "file headers": file.headers}
 
 @app.post("/files/")
 async def create_file(file: bytes = File()):
@@ -11,3 +14,4 @@ async def create_file(file: bytes = File()):
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}
+    
---
+++ 73_request_files_metadata.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+# AGREGAR METADATA A LA SECCION DE UPLOAD FILE
+
+@app.post("/files/")
+async def create_file(file: bytes = File(description="A file read as bytes")):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(description="A file read as UploadFile")):
+    return {"filename": file.filename}
