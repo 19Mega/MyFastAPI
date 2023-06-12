5d6547c913ee63ba9abf4a8820f71fc3b81b3728
Matias <matiasborba89@gmail.com>
2023-06-12 18:37:36 -0300

    learning +

---
+++ 84_config_status_code.py
@@ -0,0 +1,20 @@
+
+# Configuración de una operación de ruta
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+
+
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    return item
